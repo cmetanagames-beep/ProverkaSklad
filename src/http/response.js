9b1b474c4d9c43e01b2e87bf189909ac10b6e726
cmetanagames-beep <cmetanagames@gmail.com>
2026-08-10 14:43:50 +0300

    Add server module: src/http/response.js

---
+++ src/http/response.js
@@ -0,0 +1,18 @@
+function sendJson(res, status, payload, headers = {}) {
+  res.writeHead(status, { 'Content-Type': 'application/json; charset=utf-8', 'Cache-Control': 'no-store', ...headers });
+  res.end(JSON.stringify(payload));
+}
+
+async function readJson(req, maxBytes = 64 * 1024) {
+  const chunks = [];
+  let size = 0;
+  for await (const chunk of req) {
+    size += chunk.length;
+    if (size > maxBytes) throw new Error('BODY_TOO_LARGE');
+    chunks.push(chunk);
+  }
+  return JSON.parse(Buffer.concat(chunks).toString('utf8') || '{}');
+}
+
+module.exports = { sendJson, readJson };
+
